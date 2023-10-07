a15f18d773dedb7f353269a7356f3aa17e1737c5
belbix <7453635@gmail.com>
2023-10-07 14:21:52 +0300

    add gov address

--- scripts/addresses/BaseAddresses.ts
+++ scripts/addresses/BaseAddresses.ts
@@ -3,9 +3,7 @@
 export class BaseAddresses {
 
   public static ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
-  public static GOV_ADDRESS = "".toLowerCase();
-  public static LIQUIDATOR = "".toLowerCase();
-  public static VAULT_IMPLEMENTATION = "0xA81d2B4f9Fa3ffc8Ce7b415562449c1271B9C9C9".toLowerCase();
+  public static GOV_ADDRESS = "0x4E08441Bf2838dc32C9B598538A3Cb62f97D2453".toLowerCase();
 
 
   public static WETH_TOKEN = "0x4200000000000000000000000000000000000006".toLowerCase();
